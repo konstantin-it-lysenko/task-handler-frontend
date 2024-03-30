1200d5f5174b426195f2ee4b3f416a8d082f5665
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-30 12:41:46 +0200

    add: pages url config

---
+++ src/config/pages-url.config.ts
@@ -0,0 +1,10 @@
+class DASHBOARD {
+    private root = '/i'
+
+    HOME = this.root
+    TASKS = `${this.root}/tasks`
+    HABITS = `${this.root}/habits`
+    TIME_BLOCKING = `${this.root}/time-blocking`
+}
+
+export const DASHBOARD_PAGES = new DASHBOARD()
