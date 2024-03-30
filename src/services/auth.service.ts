2a31470e7065edd82ffcf08549efd0e8af8433ed
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-31 00:26:34 +0200

    add: get access token auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -11,5 +11,15 @@ export const authService = {
         if (response.data.accessToken) saveTokenStorage(response.data.accessToken)
         
         return response
+    },
+
+    async getNewTokens() {
+        const response = await axiosInstance.post<IAuthResponse>(
+            `/auth/login/access-token`
+        )
+
+        if (response.data.accessToken) saveTokenStorage(response.data.accessToken)
+        
+        return response
     }
 }
