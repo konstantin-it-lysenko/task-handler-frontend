91014afe3602ac6f2a55bd56ad710cd9ba6d8952
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-31 00:30:05 +0200

    add: axios get new token if expired or missed

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -1,5 +1,7 @@
-import { getAccessToken } from '@/services/auth-token.service';
+import { getAccessToken, removeFromStorage } from '@/services/auth-token.service';
 import axios, { type CreateAxiosDefaults } from 'axios';
+import { errorCatch } from './error';
+import { authService } from '@/services/auth.service';
 
 const options: CreateAxiosDefaults = {
     baseURL: 'http://localhost:3000/api',
@@ -22,3 +24,30 @@ axiosWithAuth.interceptors.request.use(config => {
     return config
 })
 
+axiosWithAuth.interceptors.response.use(
+    config => config,
+    async error => {
+        const originalRequest = error.config
+
+        if (
+            (error?.response?.status === 401 ||
+            errorCatch(error) === 'jwt expired' ||
+            errorCatch(error) === 'jwt must be provided') &&
+            error.config &&
+            !error.config._isRetry
+        ) {
+            originalRequest._isRetry = true
+            try {
+                await authService.getNewTokens()
+
+                return axiosWithAuth.request(originalRequest)
+            } catch (error) {
+                if(errorCatch(error) === 'jwt expired') removeFromStorage()
+            }
+        }
+    
+        throw error
+    }
+)
+
+export {axiosInstance, axiosWithAuth}
