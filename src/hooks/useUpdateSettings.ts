36766ef606dc679f99cdb6a7c01d5c411cc83389
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-04-01 01:48:33 +0300

    add: update settings hook

---
+++ src/hooks/useUpdateSettings.ts
@@ -0,0 +1,22 @@
+'use client'
+
+import { userService } from "@/services/user.service"
+import { TypeUserForm } from "@/types/auth.types"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { toast } from "sonner"
+
+export function useUpdateSettings() {
+
+    const queryClient = useQueryClient()
+
+    const { mutate, isPending } = useMutation({
+        mutationKey: ['update profile'],
+        mutationFn: (data: TypeUserForm) => userService.update(data),
+        onSuccess() {
+            toast.success('Profile successfully updated!')
+            queryClient.invalidateQueries({ queryKey: ['profile'] })
+        }
+    })
+
+    return { mutate, isPending }
+}
