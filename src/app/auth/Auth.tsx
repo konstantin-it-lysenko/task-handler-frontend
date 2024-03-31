64765324e50d75fcf9422bad3cec8f58f5a130f1
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-31 18:40:10 +0300

    add: auth register form markup

--- src/app/auth/Auth.tsx
+++ src/app/auth/Auth.tsx
@@ -1,5 +1,43 @@
 'use client'
+import { DASHBOARD_PAGES } from '@/config/pages-url.config';
+import { authService } from '@/services/auth.service';
+import { IAuthForm } from '@/types/auth.types';
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+
 
 export const Auth = () => {
-  return <div>Auth</div>
+    const {register, handleSubmit, reset } = useForm<IAuthForm>({
+        mode: 'onChange'
+    })
+    const [isLoginForm, setIsLoginForm] = useState(false)
+    const { push } = useRouter()
+    
+    const { mutate } = useMutation({
+        mutationKey: ['auth'],
+        mutationFn: (data: IAuthForm) => authService.main(isLoginForm ? 'login' : 'register', data),
+        onSuccess() {
+            toast.success('Successfully login!')
+            reset()
+            push(DASHBOARD_PAGES.HOME)
+        }
+    })
+
+    const onSubmit: SubmitHandler<IAuthForm> = data => {
+        mutate(data)
+    }
+
+    return (<div className='flex min-h-screen'>
+        <form className='w-1/4 m-auto shadow bg-sidebar rounded-xl p-layout'>
+            {/* Heading */}
+            {/* Fields */}
+            <div className='flex items-center gap-5 justify-center'>
+                {/* Buttons */}
+                AUTH REGISTER FORM
+            </div>
+        </form>
+    </div>)
 }
