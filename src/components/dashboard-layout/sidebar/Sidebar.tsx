2bae310533e233374b9ea1984d83890c415b4fc5
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-04-01 01:38:23 +0300

    complete: sidebar logout/footer

--- src/components/dashboard-layout/sidebar/Sidebar.tsx
+++ src/components/dashboard-layout/sidebar/Sidebar.tsx
@@ -1,7 +1,50 @@
+import { COLORS } from "@/constants/color.constants";
+import { GanttChartSquare } from "lucide-react";
+import Link from "next/link";
+import { LogoutButton } from "./LogoutButton";
+import { MENU } from "./menu.data";
+import { MenuItem } from "./MenuItem";
+
 export function Sidebar() {
-  return (
-    <div>
-      Sidebar
-    </div>
-  )
+    return <aside className="border-r border-r-border h-full bg-sidebar flex flex-col justify-between">
+        <div>
+            <Link
+                href='/'
+                className="flex items-center gap-2.5 p-layout border-b border-b-border"
+            >
+                <GanttChartSquare
+                    color={COLORS.primary}
+                    size={38}
+                />
+                <span className="text-2xl font-bold relative">
+                    TASK Handler
+                    <span className="absolute -top-1 -right-6 text-xs opacity-40 rotate-[18deg] font-normal">
+                        beta
+                    </span>
+                </span>
+            </Link>
+            <div className="p-3 relative">
+                <LogoutButton />
+                {MENU.map(item => (
+                    <MenuItem
+                        key={item.link}
+                        item={item}
+                    />
+                ))}
+            </div>
+        </div>
+
+        <footer className="text-xs opacity-40 font-normal text-center p-layout">
+            2024 &copy; With love from {' '}
+            <a
+                href="https://www.linkedin.com/in/constantine-it-lysenko/"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:text-primary text-brand-300 transition-colors"
+            >
+                Konstantin L.
+            </a>
+                <br /> All rights reserved.
+        </footer>
+    </aside>
 }
