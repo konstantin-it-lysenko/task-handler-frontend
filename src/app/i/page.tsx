6912842a3c2d7bf50958ffa473f5411f6d20a770
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-04-01 00:06:31 +0300

    add: stats(dashboard) page

--- src/app/i/page.tsx
+++ src/app/i/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 
 import { NO_INDEX_PAGE } from "@/constants/seo.constants";
+import { Heading } from "@/components/ui/Heading";
+import Statistics from "./Statistics";
 
 export const metadata: Metadata = {
     title: 'Dashboard',
@@ -8,6 +10,9 @@ export const metadata: Metadata = {
 }
 
 export default function DashboardPage() {
-    return <div>Dashboard</div>
+    return <div>
+        <Heading title='Statistics' />
+        <Statistics />
+    </div>
 }
 
