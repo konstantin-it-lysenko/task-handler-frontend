03a91189eda7f90bad3db99dc7ce39d737a66daf
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-04-01 01:37:38 +0300

    update: add settings route config

--- src/config/pages-url.config.ts
+++ src/config/pages-url.config.ts
@@ -3,8 +3,9 @@ class DASHBOARD {
 
     HOME = this.root
     TASKS = `${this.root}/tasks`
-    HABITS = `${this.root}/habits`
+    // HABITS = `${this.root}/habits`
     TIME_BLOCKING = `${this.root}/time-blocking`
+    SETTINGS = `${this.root}/settings`
 }
 
 export const DASHBOARD_PAGES = new DASHBOARD()
