79c4a2bf0bfcecbb268f398a758129ec9615df3b
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-04-01 18:21:05 +0300

    add: child time-blocking component

--- src/app/i/time-blocking/page.tsx
+++ src/app/i/time-blocking/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 
 import { NO_INDEX_PAGE } from "@/constants/seo.constants";
 import { Heading } from "@/components/ui/Heading";
+import { TimeBlocking } from "./TimeBlocking";
 
 export const metadata: Metadata = {
     title: 'Time Blocking',
@@ -11,6 +12,6 @@ export const metadata: Metadata = {
 export default function TimeBlockingPage() {
     return <div>
         <Heading title='Time blocking' />
-        {/* <TimeBlocking /> */}
+        <TimeBlocking />
     </div>
 }
