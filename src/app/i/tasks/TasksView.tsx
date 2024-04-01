e2f36b6a0ea0c8031424bb8d8560bddc6efce237
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-04-01 16:10:17 +0300

    add: tasks list/kanban view options

--- src/app/i/tasks/TasksView.tsx
+++ src/app/i/tasks/TasksView.tsx
@@ -1,9 +1,24 @@
 'use client'
 
+import { useLocalStorage } from "@/hooks/useLocalStorage"
 import { ListView } from "./list-view/ListView"
+import Loader from "@/components/ui/Loader"
+import { SwitcherView } from "./SwitcherView"
+import { KanbanView } from "./kanban-view/KanbanView"
 
 export type TypeView = 'list' | 'kanban'
 
 export function TasksView() {
-  return <ListView />
+  const [type, setType, isLoading] = useLocalStorage<TypeView>({
+    key: 'view-type',
+    defaultValue: 'list'
+  })
+
+  if (isLoading) return <Loader />
+
+  return <div>
+    <SwitcherView setType={setType} type={type} />
+    {type === 'list' ? <ListView /> : <KanbanView />}
+    <ListView />
+  </div>
 }
